e2f7194fbf919b072f2d1c740aff9e590f499812
Rishabh Raj Shukla <67179616+rajshukla1102@users.noreply.github.com>
2020-08-01 17:28:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -64,17 +64,3 @@ search.addEventListener('input',function(){
         }
     })
 })
-// `<div class="card">
-//             <div class="card-header" id="heading${index}">
-//                 <h2 class="mb-0">
-//                 <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
-//                     aria-expanded="false" aria-controls="collapse${index}">
-//                    <b>Breaking News ${index+1}:</b> ${element["title"]}
-//                 </button>
-//                 </h2>
-//             </div>
-
-//             <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
-//                 <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
-//             </div>
-//         </div>`;
